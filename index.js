728b5b5af87ed08146be2369d2d623958ba24f95
romanZhurakivskyi <72511388+roman1923@users.noreply.github.com>
2022-11-20 22:21:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
-$(function() {
-    $( "#PINform" ).draggable();
+$(function(){
+    $('.enter').click(function(){
+        alert($( ".input" ).val());
+    });
+    $('body').on('click', '.password-control', function(){
+        if ($('#password-input').attr('type') == 'password'){
+            $(this).html('Hide');
+            $('#password-input').attr('type', 'text');
+        } else {
+            $(this).html('Show');
+            $('#password-input').attr('type', 'password');
+        }
+        return false;
+    }); 
 });
-
-$( "#PINcode" ).html(
-    "<form action='' method='' name='PINform' id='PINform' autocomplete='off' draggable='true'>" +
-        "<input id='PINbox' type='password' inputmode='numeric' minlength='4' maxlength='4' value='' name='PINbox' enabled />" +
-        "<br/>" +
-        "<input type='button' class='PINbutton' name='1' value='1' id='1' onClick=addNumber(this); />" +
-        "<input type='button' class='PINbutton' name='2' value='2' id='2' onClick=addNumber(this); />" +
-        "<input type='button' class='PINbutton' name='3' value='3' id='3' onClick=addNumber(this); />" +
-        "<br>" +
-        "<input type='button' class='PINbutton' name='4' value='4' id='4' onClick=addNumber(this); />" +
-        "<input type='button' class='PINbutton' name='5' value='5' id='5' onClick=addNumber(this); />" +
-        "<input type='button' class='PINbutton' name='6' value='6' id='6' onClick=addNumber(this); />" +
-        "<br>" +
-        "<input type='button' class='PINbutton' name='7' value='7' id='7' onClick=addNumber(this); />" +
-        "<input type='button' class='PINbutton' name='8' value='8' id='8' onClick=addNumber(this); />" +
-        "<input type='button' class='PINbutton' name='9' value='9' id='9' onClick=addNumber(this); />" +
-        "<br>" +
-        "<input type='button' class='PINbutton' name='0' value='0' id='0' onClick=addNumber(this); />" +
-        "<br>"+
-        "<input type='button' class='PINbutton cancel' value='cancel' id='-' onClick=cancelForm(PINbox); />" +
-        "<input type='button' class='PINbutton clear' name='-' value='clear' id='-' onClick=clearForm(this); />" +
-        "<input type='button' class='PINbutton enter' name='+' value='enter' id='+' onClick=submitForm(PINbox); />" +
-    "</form>"
-);
-
 function addNumber(e){
-    var v = $( "#PINbox" ).val();
-    $( "#PINbox" ).val( v + e.value );
+    var v = $( ".input" ).val();
+    $( ".input" ).val( v + e.value );
 }
 function clearForm(e){
-    $( "#PINbox" ).val( "" );
+    $( ".input" ).val( "" );
 }
-function submitForm(e) {
-    if (e.value == "") {
-        alert("Enter a PIN");
-    } else if (e.value !== ""){
-        alert("Ok");
-    };
-};
 function cancelForm(e){
-    $( "#PINbox" ).val(PINbox.length -1);
-}
+    var $myInput = $('.input');
+    $myInput.val($myInput.val().slice(0, -1));
+ }
+ 
+ 
